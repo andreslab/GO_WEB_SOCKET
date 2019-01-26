061f91b26a2bab071ce49a4e41ccb9529f860aab
Jaime Andrade <jaimeandrade454@gmail.com>
2019-01-26 14:07:18 -0500

    conexion webscoket para los eventos del juegador

---
+++ src/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.HandleFunc("/v1/ws", func(w http.ResponseWriter, r *http.Request) {
+		var conn, _ = upgrader.Upgrade(w, r, nil)
+		go func(conn *websocket.Conn) {
+			for {
+				mType, msg, _ := conn.ReadMessage()
+
+				conn.WriteMessage(mType, msg)
+			}
+		}(conn)
+	})
+
+	http.HandleFunc("/v2/ws", func(w http.ResponseWriter, r *http.Request) {
+		var conn, err = upgrader.Upgrade(w, r, nil)
+
+		if err != nil {
+			log.Println("Error while reading JSON from websocket ", err.Error())
+		}
+
+		go func(conn *websocket.Conn) {
+			for {
+				_, msg, _ := conn.ReadMessage()
+				println(string(msg))
+			}
+		}(conn)
+	})
+
+	http.HandleFunc("/v3/ws", func(w http.ResponseWriter, r *http.Request) {
+		var conn, _ = upgrader.Upgrade(w, r, nil)
+		go func(conn *websocket.Conn) {
+			ch := time.Tick(5 * time.Second)
+
+			for range ch {
+				conn.WriteJSON(myStruct{
+					Username:  "mvansickle",
+					FirstName: "Michael",
+					LastName:  "Van Sickle",
+				})
+			}
+		}(conn)
+	})
+
+	http.HandleFunc("/v4/ws", func(w http.ResponseWriter, r *http.Request) {
+		var conn, _ = upgrader.Upgrade(w, r, nil)
+		go func(conn *websocket.Conn) {
+			for {
+				_, _, err := conn.ReadMessage()
+				if err != nil {
+					conn.Close()
+				}
+			}
+		}(conn)
+
+		go func(conn *websocket.Conn) {
+			ch := time.Tick(5 * time.Second)
+
+			for range ch {
+				conn.WriteJSON(myStruct{
+					PosX:   "4",
+					PosY:   "7",
+					Action: "card_1",
+					Status: "Activo",
+				})
+			}
+		}(conn)
+	})
+
+	http.ListenAndServe(":3000", nil)
+}
+
+type myStruct struct {
+	PosX   string `json:"pos_x"`
+	PosY   string `json:"pos_y"`
+	Action string `json:"action"`
+	Status string `json:"status"`
+}
